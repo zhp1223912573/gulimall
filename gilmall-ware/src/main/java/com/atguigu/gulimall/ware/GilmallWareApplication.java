40a83ee110e160b21269b26aaf5c58591f873077
zhp <zhp1223912573@qq.com>
2023-01-09 02:47:32 +0800

    项目结构初始创建

---
+++ gilmall-ware/src/main/java/com/atguigu/gulimall/ware/GilmallWareApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GilmallWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GilmallWareApplication.class, args);
+    }
+
+}
